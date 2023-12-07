21d7f1857ca9be939cfe69064766a27a503e62a0
Daniel Leal <danielgek@gmail.com>
2023-12-07 14:01:46 +0000

    update ui

--- agency_swarm/agency/agency.py
+++ agency_swarm/agency/agency.py
@@ -97,7 +97,7 @@ class Agency:
                     for bot_message in gen:
                         if bot_message.sender_name.lower() == "user":
                             continue
-                        print(bot_message)
+                        print(bot_message.sender_name)
                         message = bot_message.get_sender_emoji() + " " + bot_message.get_formatted_content()
 
                         history.append((None, message))
